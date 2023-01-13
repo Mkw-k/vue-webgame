efb928a65595507e2afc8523a173b5baef585385
Mkw-k <rhauddn111@hanmail.net>
2023-01-13 18:36:57 +0900

    studying vue

--- 3.숫자야구/main.js
+++ 3.숫자야구/main.js
@@ -1,4 +1,3 @@
 import Vue from 'vue';
-import NumberBaseball from './NumberBaseball';
 
-new Vue(NumberBaseball).$mount('#root');
+new Vue().$mount('#root');
--- 3.숫자야구/webpack.config.js
+++ 3.숫자야구/webpack.config.js
@@ -1,26 +1,18 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const path = require('path');
+//스크립트가 너무 많아질경우 하나로 합치기 위해서 사용 [웹팩]
+const paht = require("path");
 
 module.exports = {
-  mode: 'development',
-  devtool: 'eval',
-  resolve: {
-    extensions: ['.js', '.vue'],
-  },
-  entry: {
-    app: path.join(__dirname, 'main.js'),
-  },
-  module: {
-    rules: [{
-      test: /\.vue$/,
-      use: 'vue-loader',
-    }],
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-  ],
-  output: {
-    filename: '[name].js',
-    path: path.join(__dirname, 'dist'),
-  },
+    entry : {
+        app : path.join(__dirname, 'main.js'), //하나로 합쳐질 파일의 이름 -> app.js
+    },
+    module : {
+        rules : [{
+
+        }]
+    },
+    plugins : [], 
+    output : {
+        filename : '[name].js',
+        path : path.join(__dirname, 'dist'),
+    },
 };
